12797b6634aabec8735b76283e651646b6c2ac5a
Tanishq070707 <66459762+Tanishq2730@users.noreply.github.com>
2024-06-06 12:02:42 +0530

    latest code

---
+++ data.js
@@ -0,0 +1,195 @@
+const industries = [
+  {
+    title: "Adhesive & Sealants",
+    id: "40248964-a573-4d59-8bb2-fb3c32366186",
+    icon: "./src/img/appicon/1.svg",
+    details: {
+      heading: "ADHESIVE & SEALANTS",
+      subHeading: "About Adhesive & Sealants Industry",
+      content:
+        "Talc is the world’s softest and most hydrophobic mineral – insoluble and inert. It adds new features to products and makes processes more efficient. Talc grades on the market often contain Magnesium Carbonate, Dolomite, Calcite, Quartz or Chlorite. The properties of these minerals may vary depending on the resource. 20 Microns has access to some of the finest quality Talc reserves in India and gives it an edge over the other struggling resources world over.",
+      image: "./src/img/industry/1.png",
+    },
+  },
+  {
+    title: "Automobile",
+    id: "f623363a-0a07-454c-94bb-da815f239a62",
+    icon: "./src/img/appicon/2.svg",
+    details: {
+      heading: "Automobile",
+      subHeading: "About Automobile Industry",
+      content:
+        "Talc is the world’s softest and most hydrophobic mineral – insoluble and inert. It adds new features to products and makes processes more efficient. Talc grades on the market often contain Magnesium Carbonate, Dolomite, Calcite, Quartz or Chlorite. The properties of these minerals may vary depending on the resource. 20 Microns has access to some of the finest quality Talc reserves in India and gives it an edge over the other struggling resources world over.",
+      image: "./src/img/industry/1.png",
+    },
+  },
+  {
+    title: "Cosmetic",
+    id: "3c09a996-9931-4278-bc21-17a3131c3fed",
+    icon: "./src/img/appicon/3.svg",
+  },
+  {
+    title: "Ceramic",
+    id: "e55121d7-f05c-453d-b9d4-bb216b0845be",
+    icon: "./src/img/appicon/4.svg",
+  },
+  {
+    title: "Building Additives/Fillers",
+    id: "2d70bbfa-0c87-4f84-8823-befabedc05ef",
+    icon: "./src/img/appicon/5.svg",
+  },
+  {
+    title: "Detergents & Soaps",
+    id: "ab2c9fd9-7ee5-4f7c-a309-e9ca65d372e2",
+    icon: "./src/img/appicon/6.svg",
+  },
+  {
+    title: "Masterbatch Filler",
+    id: "0462f7de-83fa-4697-a0e7-98b8d6634529",
+    icon: "./src/img/appicon/7.svg",
+  },
+  {
+    title: "Paper & Boards",
+    id: "f469524b-4574-40ce-a2ac-73968332e285",
+    icon: "./src/img/appicon/8.svg",
+  },
+  {
+    title: "Paints & Coatings",
+    id: "24f52596-4a92-4dfc-97c4-cef0425bc026",
+    icon: "./src/img/appicon/9.svg",
+  },
+  {
+    title: "Plastic & Polymer",
+    id: "cdba2271-c455-449b-9a1b-9f98b96cabc0",
+    icon: "./src/img/appicon/10.svg",
+  },
+  {
+    title: "Rubber",
+    id: "5fd0eeac-54f6-40bf-8fa6-6cfaf91bbe06",
+    icon: "./src/img/appicon/11.svg",
+  },
+  {
+    title: "Textile",
+    id: "213420c3-9c59-4643-85b1-1b6275e16fc0",
+    icon: "./src/img/appicon/12.svg",
+  },
+  {
+    title: "Semiconductors/Electronics",
+    id: "4489a0ba-0ee8-4434-a91a-01f786902a31",
+    icon: "./src/img/appicon/13.svg",
+  },
+  {
+    title: "Pharma & Drugs",
+    id: "0ea2bff7-0288-4fd0-b22d-ef29ea5dbf44",
+    icon: "./src/img/appicon/14.svg",
+  },
+  {
+    title: "Cables & Wire",
+    id: "763d6e48-b4bd-4f25-b473-e2c5bf0cf7b0",
+    icon: "./src/img/appicon/15.svg",
+  },
+  {
+    title: "Polymer Masterbatch Additives",
+    id: "5ed00108-eda8-4284-b8a8-006ed4d68a14",
+    icon: "./src/img/appicon/16.svg",
+  },
+];
+
+const products = [
+  {
+    title: "Talc",
+    id: "497b07ea-97c4-4ad2-9a1b-b5ffff1dfa02",
+    image: "./src/img/product/1.png",
+    industries: [
+      "40248964-a573-4d59-8bb2-fb3c32366186",
+      "f623363a-0a07-454c-94bb-da815f239a62",
+      "3c09a996-9931-4278-bc21-17a3131c3fed",
+    ],
+    details: {
+      heading: "Talc",
+      content:
+        "Talc is the world’s softest and most hydrophobic mineral – insoluble and inert. It adds new features to products and makes processes more efficient. Talc grades on the market often contain Magnesium Carbonate, Dolomite, Calcite, Quartz or Chlorite. The properties of these minerals may vary depending on the resource. 20 Microns has access to some of the finest quality Talc reserves in India and gives it an edge over the other struggling resources world over.",
+      image: "./src/img/industry/1.png",
+      structureImage:"./src/img/Structure/1.png",
+      structreContent:"The people of mineral mining with purpose to maximize efficiencies and minimize our environmental footprint .",
+    },
+  },
+  {
+    title: "Calcium Carbonate",
+    id: "67a75c26-74d6-4571-a528-bcdfa2930907",
+    image: "./src/img/product/1.png",
+    industries: [
+      "40248964-a573-4d59-8bb2-fb3c32366186",
+      "f623363a-0a07-454c-94bb-da815f239a62",
+      "3c09a996-9931-4278-bc21-17a3131c3fed",
+    ],
+    details: {
+      heading: "CALCIUM CARBONATE",
+      content:
+        "Talc is the world’s softest and most hydrophobic mineral – insoluble and inert. It adds new features to products and makes processes more efficient. Talc grades on the market often contain Magnesium Carbonate, Dolomite, Calcite, Quartz or Chlorite. The properties of these minerals may vary depending on the resource. 20 Microns has access to some of the finest quality Talc reserves in India and gives it an edge over the other struggling resources world over.",
+      image: "./src/img/industry/1.png",
+      structureImage:"./src/img/Structure/1.png",
+      structreContent:"The people of mineral mining with purpose to maximize efficiencies and minimize our environmental footprint .",
+    },
+  },
+  {
+    title: "Polymer Masterbatch Additives",
+    id: "964b04bc-4842-4771-aa16-3bf91baf5867",
+    image: "./src/img/product/1.png",
+    industries: [],
+  },
+  {
+    title: "Polymer Masterbatch Additives",
+    id: "65c50094-0ffc-4734-83ed-c71065e27b69",
+    image: "./src/img/product/1.png",
+    industries: [],
+  },
+  {
+    title: "Polymer Masterbatch Additives",
+    id: "7b3df0fc-d1f1-4aac-96be-7a65dfc4ed74",
+    image: "./src/img/product/1.png",
+    industries: [],
+  },
+  {
+    title: "Polymer Masterbatch Additives",
+    id: "64605ba5-8587-4276-af0a-4b0f6b659c23",
+    image: "./src/img/product/1.png",
+    industries: [],
+  },
+  {
+    title: "Polymer Masterbatch Additives",
+    id: "539f3aef-6dc8-49f7-a8c5-4a16dcb97f3a",
+    image: "./src/img/product/1.png",
+    industries: [],
+  },
+  {
+    title: "Polymer Masterbatch Additives",
+    id: "848138bf-ff13-4e89-adf7-74cc8cdeb47a",
+    image: "./src/img/product/1.png",
+    industries: [],
+  },
+  {
+    title: "Polymer Masterbatch Additives",
+    id: "07058995-9787-46fc-a541-8d3476315a22",
+    image: "./src/img/product/1.png",
+    industries: [],
+  },
+  {
+    title: "Polymer Masterbatch Additives",
+    id: "3cbb6ea5-826a-420e-9427-fe7e77526d12",
+    image: "./src/img/product/1.png",
+    industries: [],
+  },
+  {
+    title: "Polymer Masterbatch Additives",
+    id: "6ab4a8e4-000a-43f3-bff5-7368640f6dfc",
+    image: "./src/img/product/1.png",
+    industries: [],
+  },
+  {
+    title: "Polymer Masterbatch Additives",
+    id: "53a06155-edd3-4f7a-9999-971c6b17adc2",
+    image: "./src/img/product/1.png",
+    industries: [],
+  },
+];
